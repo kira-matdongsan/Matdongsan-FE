46947619e73ad99229c7f5a7bcc677b999ba4060
YYJ-1229 <90436@naver.com>
2025-05-25 17:49:19 +0900

    📌 : 배포 파일 삭제

--- storybook-static/assets/Title.stories-BeU2porT.js
+++
@@ -1,6 +0,0 @@
-import{j as d}from"./jsx-runtime-D_zvdyIk.js";const l=({size:r="medium",label:m,...i})=>{const o="color-[#343A3F]",n={small:"w-[16px] h-[16px]",medium:"w-[37px] h-[20px] text-[14px]",large:"w-[32px] h-[32px]"};return d.jsx("p",{className:`${o} ${n[r]}`,...i,children:m})};l.__docgenInfo={description:"",methods:[],displayName:"Title",props:{size:{required:!1,tsType:{name:"union",raw:'"small" | "medium" | "large"',elements:[{name:"literal",value:'"small"'},{name:"literal",value:'"medium"'},{name:"literal",value:'"large"'}]},description:"",defaultValue:{value:'"medium"',computed:!1}},label:{required:!1,tsType:{name:"string"},description:""}}};const u={title:"Components/Title",component:l,tags:["autodocs"]},e={args:{size:"medium",label:"옥수수"}};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    size: "medium",
-    label: "옥수수"
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const c=["Medium"];export{e as Medium,c as __namedExportsOrder,u as default};
